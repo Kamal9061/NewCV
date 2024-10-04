9516710d5592ffa19e73a7f08530f7c937079bca
Kamal Jeet Arya <96496716+Kamal9061@users.noreply.github.com>
2024-10-04 13:07:23 +0530

    first commit

---
+++ src/Schemas/Data.js
@@ -0,0 +1,120 @@
+export const Data = {
+  profile: {
+    name: "Kamal Jeet Arya",
+    ocupation: "Sr. Software Engineer",
+    location: "Noida, India",
+    email: "kamalkhokher12341234@gmail.com",
+    telephone: "+91 9068428961",
+    image: "images/me.jpg",
+  },
+  aboutMe: {
+    label: "Profile",
+    description:
+      "Building scalable and maintainable systems with a focus on clean architecture and design. Learning backend-end and devops.",
+  },
+  skills: {
+    technicalLabel: "Technologies",
+    softLabel: "Skills",
+    technicalSkills: [
+      "JavaScript",
+      "React",
+      "Next",
+      "Express",
+      "Nodejs",
+      "MongoDB",
+      "Redis",
+      "Git",
+      "AWS",
+      "Docker",
+      "Jira",
+    ],
+    softSkills: [
+      "Ownership",
+      "Mentoring"
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Visit my LinkedIn",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/kamal9068/",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "Visit my GitHub",
+        name: "github",
+        url: "https://github.com/Kamal9061",
+        className: "bxl-github",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "Sr. Software Engineer",
+        period: "April. 2023 - Present",
+        company: "Plutos ONE",
+        description: [
+          "Developed a schema-driven UI for contracts management, enabling payers and providers to input their terms and compare benefits. This UI dynamically generates the appropriate interface elements based on the schema provided by the backend.",
+          "Developing software solutions by studying information needs, conferring with users, studying systems flow,data usage, and work processes",
+          "Integrated payment gateways such as PayU and Razorpay for secure transactions.",
+          "Established a unified login and registration flow for all campaigns and internal tools using Firebase authentication",
+          "Implemented BBPS (Bharat Bill Payment System) for bill payments provided by NPCI, ensuring seamless payment processes."
+        ],
+      },
+      {
+        title: "Associate Software Engineer",
+        period: "May. 2022 - Apr. 2023",
+        company: "Speqto Technologies",
+        description: [
+          "Collaborated with the team to develop a bulk solution for outreach, increasing our capability from 10k/day to 10k per hour. The project involved creating resiliency and self-healing solutions for async tasks, developing bulk support for APIs while ensuring backward compatibility, and standardising/benchmarking pods cpu/memory limit & request.",
+          "Integrated communication solutions with vendors such as PostGrid for letters and Twilio for voice/SMS.",
+          "Developed health & sanity checks for a seamless deployment process on a new customer."
+        ],
+      },
+      {
+        title: "Software Developer Intern",
+        period: "Sep 2021 - May. 2022",
+        company: "Crio",
+        description: [
+          " Conducted instructional sessions on MERN stack. Resolved student queries in collaboration with engineering and content delivery teams."
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Bachelor of Engineering in Computer Science & Engineering",
+        date: "2017-2021",
+        institution: "Dr. Abdul Kalam Technical University, Lucknow",
+      },
+    ],
+    Projects: [
+      {
+        name: "Hospital Management System",
+        company: "",
+        period: "",
+        description: [
+          "A progressive web app for both doctors and patients. Medico, where people can get monitored by the doctor and consult for better health",
+        ],
+      },
+      {
+        name: "Quiz Application",
+        company: "",
+        period: "",
+        description: [
+          "Developed a website where a user play quiz questions and get score. Tech stack- React Js , Java Script , Git and MongoDB.",
+        ],
+      },
+      {
+        name: "Restorant App",
+        company: "",
+        period: "",
+        description: [
+          "Developed features like login, signup, sending emails, payment gateway, post feeds & chatbox using websocket. Tech stack- Nodejs & MongoDB",
+        ],
+      }
+    ],
+  },
+};
